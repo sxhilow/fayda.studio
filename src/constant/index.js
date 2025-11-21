ac47b2441a23a64eb8385da36e4a54c1ff07e31c
Sahil Jada <jadasahil786@gmail.com>
2025-11-21 12:12:17 +0200

    feat: add responsive header and update main layout

---
+++ src/constant/index.js
@@ -0,0 +1,6 @@
+export const navItems = [
+  { href: '#', label: 'Home' },
+  { href: '/services', label: 'Services' },
+  { href: '#plans', label: 'Plans' },
+  { href: '#faqs', label: 'FAQs' },
+];
