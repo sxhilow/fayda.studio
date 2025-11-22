ed441dd1a12cfcfe2334d2c5124995427f5cb5de
Sahil Jada <jadasahil786@gmail.com>
2025-11-22 12:51:43 +0200

    feat: add responsive service section

--- src/constant/index.js
+++ src/constant/index.js
@@ -4,3 +4,27 @@ export const navItems = [
   { href: '#plans', label: 'Plans' },
   { href: '#faqs', label: 'FAQs' },
 ];
+
+export const services = [
+  {
+    number: "01",
+    title: "UI/UX Design",
+    tags: ["Design", "Revision", "Analysis"],
+    description: "Our UI/UX focuses on clarity: clean layouts, intuitive flows, and designs that help users take action without thinking. We remove unnecessary friction, structure information logically, and build interfaces that guide customers exactly where they need to go.",
+    bg: "#D7F1FF"
+  },
+  {
+    number: "02",
+    title: "Web Development",
+    tags: ["Dev", "Support", "Maintainence"],
+    description: "Our web development turns those designs into fast, functional websites that actually perform. We focus on clean code, responsive layouts, and pages that load instantly. Every build emphasizes speed, accessibility, and reliability, giving your business a website that looks sharp and works flawlessly on every device.",
+    bg: "#FFE7C2"
+  },
+  {
+    number: "03",
+    title: "Custom Web App",
+    tags: ["Backend", "APIs", "Scalability"],
+    description: "Our web app development is built for complexity: real business logic, secure authentication, dashboards, automation, and custom features that a basic website can’t handle. We architect systems that scale, integrate with APIs, and streamline operations — turning your idea into a full product, not just a webpage.",
+    bg: "#E4D8FF"
+  },
+]
