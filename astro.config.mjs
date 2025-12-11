c5cbfc358e5c6772870f861bf1a53e3ef252d17c
Sahil Jada <jadasahil786@gmail.com>
2025-12-11 13:41:25 +0200

    feat: add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,7 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
     site: "https://fayda-studio.vercel.app",
+    integrations:[
+      sitemap()
+    ]
 });
