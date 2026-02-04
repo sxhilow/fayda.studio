704d12166b083cdb179e723e56bfbc8a396c561c
Sahil Jada <jadasahil786@gmail.com>
2026-02-04 17:32:45 +0200

    feat: add animated tech icons marquee component

--- src/data/why-us.ts
+++ src/data/why-us.ts
@@ -1,3 +1,19 @@
+import Astro from "@icons/tech-icons/Astro.svg"
+import Docker from "@icons/tech-icons/Docker.svg"
+import Figma from "@icons/tech-icons/Figma.svg"
+import Node from "@icons/tech-icons/Node.svg"
+import React from "@icons/tech-icons/React.svg"
+import Next from "@icons/tech-icons/Next.svg"
+import Typescript from "@icons/tech-icons/Typescript.svg"
+import PostgreSQL from "@icons/tech-icons/Postgres.svg"
+import MongoDB from "@icons/tech-icons/Mongodb.svg"
+import Vite from "@icons/tech-icons/Vite.svg"
+import Redux from "@icons/tech-icons/Redux.svg"
+import ESLint from "@icons/tech-icons/Eslint.svg"
+import Github from "@icons/tech-icons/Github.svg"
+import TailwindCSS from "@icons/tech-icons/Tailwind.svg"
+
+
 export const comparisonTableData = [
   {
     feature: "Tech Stack",
@@ -24,4 +40,22 @@ export const comparisonTableData = [
     us: "Yes",
     them: "Often overlooked"
   }
+];
+
+
+export const techIcons = [
+  { name: "Next.js", icon: Next },
+  { name: "TypeScript", icon: Typescript },
+  { name: "Astro", icon: Astro },
+  { name: "Tailwind CSS", icon: TailwindCSS },
+  { name: "React", icon: React },
+  { name: "PostgreSQL", icon: PostgreSQL },
+  { name: "Node.js", icon: Node },
+  { name: "Docker", icon: Docker },
+  { name: "MongoDB", icon: MongoDB },
+  { name: "Figma", icon: Figma },
+  { name: "Vite", icon: Vite },
+  { name: "Redux", icon: Redux },
+  { name: "GitHub", icon: Github },
+  { name: "ESLint", icon: ESLint },
 ];
