50a0d95545e94858ae307060678d16c0caea66c9
Sahil Jada <jadasahil786@gmail.com>
2026-04-04 21:43:32 +0200

    feat: install mdx and astro check

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from 'astro-sitemap';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
       plugins: [tailwindcss()],
     },
     site: "https://fayda.studio",
-    integrations:[
-      sitemap(),
-    ]
+    integrations:[sitemap(), mdx()]
 });
